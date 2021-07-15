61967ae028bfa63cf1e0691452f83019773ae222
tylertemporary <87210669+tylertemporary@users.noreply.github.com>
2021-07-15 10:54:01 -0700

    Update CityLandingPage.js

--- CityLandingPage.js
+++ CityLandingPage.js
@@ -1,5 +1,6 @@
 window.onclick = () => {
     console.log("fiired");
+    console.log(window.parent);
     window.parent.postMessage({
         'func': 'parentFunc',
         'message': 'Message text from iframe.'
