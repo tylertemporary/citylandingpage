854bb27eee9973b3e0ec97ce5328444f48175fa4
tylertemporary <87210669+tylertemporary@users.noreply.github.com>
2021-07-16 12:51:27 -0700

    Update CityLandingPage.js

--- CityLandingPage.js
+++ CityLandingPage.js
@@ -3,7 +3,15 @@ window.onclick = () => {
         messenger: "formBuilder",
         name: "formBuilder:postData",
         fields: {
-            company: document.getElementById('text-0000000a').value
+            company: document.getElementById('text-0000000a').value,
+            legalBusinessName: document.getElementById('text-0000000c').value,
+            taxId: document.getElementById('text-00000012').value,
+            lineOfBusiness: document.getElementById('text-00000026').value,
+            otherServices: document.getElementById('textarea-00000028').value,
+            carriers: document.getElementById('text-0000002c').value,
+            source: document.getElementById('text-0000002e').value,
+            title: document.getElementById('text-00000034').value,
+            additionalInformation: document.getElementById('textarea-0000003a').value
         }
     },'*')
 }
